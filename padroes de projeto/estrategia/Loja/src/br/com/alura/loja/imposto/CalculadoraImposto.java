325d80c9725498ed2a47e37d4f988d0bde5c2e18
Alan Araujo <alanaraujotrs@gmail.com>
2023-02-15 09:34:21 -0300

    aplicando strategy

---
+++ padroes de projeto/estrategia/Loja/src/br/com/alura/loja/imposto/CalculadoraImposto.java
@@ -0,0 +1,12 @@
+package br.com.alura.loja.imposto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class CalculadoraImposto {
+
+    public BigDecimal calcular(Orcamento orcamento, Imposto imposto){
+        return imposto.calcular(orcamento);
+    }
+}
---
+++ padroes de projeto/estrategia/Loja/src/br/com/alura/loja/imposto/Imposto.java
@@ -0,0 +1,10 @@
+package br.com.alura.loja.imposto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public interface Imposto {
+
+    BigDecimal calcular(Orcamento orcamento);
+}
