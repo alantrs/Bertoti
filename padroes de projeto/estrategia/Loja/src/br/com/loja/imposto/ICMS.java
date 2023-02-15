f9171944b2b7aec6b9ff07b4093badcee7e9b438
Alan Araujo <alanaraujotrs@gmail.com>
2023-02-15 12:12:12 -0300

    adião de comentarios

--- padroes de projeto/estrategia/Loja/src/br/com/loja/imposto/ICMS.java
+++ padroes de projeto/estrategia/Loja/src/br/com/loja/imposto/ICMS.java
@@ -4,6 +4,10 @@ import br.com.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
+/* o imposto ICMS implementa a interface,
+ * e faz seu metodo calcular de acordo com sua regra de negocio
+ */
+
 public class ICMS implements Imposto {
 
     public BigDecimal calcular(Orcamento orcamento){
