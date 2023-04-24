84fa737e9796996ef75b2aa289fd26ac8482b900
Alan Araujo <alanaraujotrs@gmail.com>
2023-04-24 12:05:15 -0300

    Singleton

---
+++ padroes de projeto/Singleton/src/AppConfig.java
@@ -0,0 +1,33 @@
+public class AppConfig {
+
+    // A instância Singleton é armazenada neste campo estático
+    private static AppConfig instance;
+
+    // As configurações são armazenadas nestes campos
+    private String appName;
+    private String appVersion;
+
+    // O construtor é declarado privado para evitar que outras classes instanciem AppConfig diretamente
+    private AppConfig() {
+        // Aqui, lemos as configurações do aplicativo de um arquivo de propriedades ou do sistema de configuração do aplicativo e armazenamos em cache nos campos 'appName' e 'appVersion'
+        appName = "Minha Aplicação";
+        appVersion = "1.0";
+    }
+
+    // O método getInstance() retorna a instância AppConfig existente ou cria uma nova, se ainda não existir
+    public static AppConfig getInstance() {
+        if (instance == null) {
+            instance = new AppConfig();
+        }
+        return instance;
+    }
+
+    // Métodos para obter as configurações do aplicativo
+    public String getAppName() {
+        return appName;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+}
---
+++ padroes de projeto/Singleton/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Obtém a instância Singleton de AppConfig
+        AppConfig config = AppConfig.getInstance();
+
+        // Obtém as configurações do aplicativo
+        String appName = config.getAppName();
+        String appVersion = config.getAppVersion();
+
+        // Usa as configurações do aplicativo
+        System.out.println("Nome do aplicativo: " + appName);
+        System.out.println("Versão do aplicativo: " + appVersion);
+    }
+}
